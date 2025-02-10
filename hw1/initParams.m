22104c26dafe5e0512d777dbbba38710d709a29f
steveGillet <stephen.gillet@gmail.edu>
2025-02-09 22:04:50 -0700

    hw2 finished, still want to finish redoing 1

--- hw1/initParams.m
+++ hw1/initParams.m
@@ -4,13 +4,38 @@ coeff1=1.332; % [Vs/rad]
 num = [-1];
 den = [coeff3, coeff2, coeff1, 0];
 format longG;
-disp(roots(den));
+poles = roots(den);
+disp(poles);
 
-Gp = 10;
-Gd = 0.1;
+figure;
+hold on;
+plot(real(poles), imag(poles), 'bx', 'MarkerSize', 10, 'LineWidth', 2);
+title('Root Locus G 0 to -1');
+xlabel('Re');
+ylabel('Im');
+grid on;
+
+Gp = -10;
+Gd = -0.1;
 numCL = [-Gd -Gp];
-denCL = [0.000266135, 2.6893656, 0.132 - Gd, -Gp];
-disp(roots(denCL));
+denCL = [coeff3, coeff2, coeff1 - Gd, -Gp];
+polesCL = roots(denCL);
+disp(polesCL);
+
+plot(real(polesCL), imag(polesCL), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+
+
+gScalar = linspace(0, -1, 100);
+for i = 1:length(gScalar)
+    g = gScalar(i);
+    denCL = [coeff3, coeff2, coeff1 - Gd*g, -Gp*g];
+    polesCL = roots(denCL);
+    plot(real(polesCL), imag(polesCL), 'k.', 'MarkerSize', 1);
+    disp(polesCL);
+end
+
+legend('Open Loop Poles', 'Closed Loop Poles', 'Root Locus');
+hold off;
 
 plot(out.closedIn.time, out.closedIn.Data, 'b-', ...
      out.closedOut.time, out.closedOut.Data, 'r--');
@@ -18,5 +43,5 @@ plot(out.closedIn.time, out.closedIn.Data, 'b-', ...
 xlabel('Time (s)');
 ylabel('Arm Angle (rads)');
 legend('Input Theta_R', 'Output Theta_L');
-title('Closed Loop Arm Angle Tracking');
+title('-1 g Closed Loop Arm Angle Tracking');
 grid on;
