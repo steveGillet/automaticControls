642ba5d61abef825f8fc0608f3db1fff49002254
steveGillet <stephen.gillet@gmail.edu>
2025-02-16 23:00:31 -0700

    hw3 part 2, replacing suspension

--- hw3/carSus.m
+++ hw3/carSus.m
@@ -4,21 +4,51 @@ m2 = 350; % [kg]
 kw = 500000; % [Nm]
 ks = 10000; % [Nm]
 
+% Original Suspension
+% A = [-b/m2 -1/m2 b/m2 0; ks 0 -ks 0; 0 0 0 -1/m1; 0 0 kw 0]; 
+% B = [0; 0; 0; -kw];       
+% C = [1 0 0 0];        
+% D = [0];  
 
-A = [-b/m2 -1/m2 b/m2 0; ks 0 -ks 0; 0 0 0 -1/m1; 0 0 kw 0]; 
-B = [0; 0; 0; -kw];       
-C = [1 0 0 0];        
+% Automatic Suspension
+A = [0 0 0; 0 0 -1/m1; 0 kw 0]; 
+B = [0 -1/m2; 0 1/m1; -kw 0];       
+C = [1 0 0];        
 D = [0];  
 
 carSusSS = ss(A, B, C, D);
+
 carSusTF = tf(carSusSS);
 disp(carSusTF);
 
-plot(wheelDisplacement.time, wheelDisplacement.Data, 'b-', ...
-     carDisplacement.time, carDisplacement.Data, 'r-');
+% plot(wheelDisplacement.time, wheelDisplacement.Data, 'b-', ...
+%      carDisplacement.time, carDisplacement.Data, 'r-');
 
+% xlabel('Time [s]');
+% ylabel('Displacement [m]');
+% legend('Wheel Displacement', 'Car Displacement');
+% title(sprintf('b = %d, Wheel vs Car Displacement',b));
+% grid on;
+
+% Auto Sus Simulation
+
+t = 0:0.01:10; 
+r = zeros(size(t)); 
+r(t >= 1) = 1;
+f1 = zeros(size(t)); 
+f1(t >= 1) = 100000;
+% f1 = 1 * sin(35.6 * 2 * pi * t);
+u = [r; f1];
+
+[y, t, x] = lsim(carSusSS, u, t);
+format longG
+disp(eig(A));
+
+% Plot the states
+figure;
+plot(t, x(:,1), 'b-', t, x(:,2), 'r-');
 xlabel('Time [s]');
-ylabel('Displacement [m]');
-legend('Wheel Displacement', 'Car Displacement');
-title(sprintf('b = %d, Wheel vs Car Displacement',b));
+ylabel('Magnitude');
+legend('Car Vertical Velocity (x_1)', 'Wheel Vertical Velocity (x_2)');
+title('Simulation of Simplified Suspension System');
 grid on;
